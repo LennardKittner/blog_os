e5f796717b096b1d420cbe83fbe6f0b45a2999b4
Lennard Kittner <lennardkittner@icloud.com>
2023-05-26 16:48:55 +0200

    First half of chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
+mod serial;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    println!("Some numbers {} and {}", 1, 1.335);
-    panic!("panic!");
+
+    #[cfg(test)]
+    test_main();
+   
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    my_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
