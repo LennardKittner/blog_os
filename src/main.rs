c16331a84579d7703817573defc7075c42ac8953
Lennard Kittner <lennardkittner@icloud.com>
2023-05-30 16:03:04 +0200

    Completed chapter 8

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     my_os::init();
+    
+    use x86_64::registers::control::Cr3;
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
