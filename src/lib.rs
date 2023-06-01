5bae36b0b2ab4004a9cbf558b7608dcc1d509d7e
Lennard Kittner <lennardkittner@icloud.com>
2023-06-01 18:14:45 +0200

    Completed chapter 9. Added the physical frame iterator to the BootInfoFrameAllocator.

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,17 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
+#![feature(type_alias_impl_trait)]
+
 
 use core::panic::PanicInfo;
+#[cfg(test)]
+use bootloader::entry_point;
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 
 pub fn init() {
     gdt::init();
@@ -51,10 +56,14 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    use bootloader::BootInfo;
+
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,16 @@
 #![test_runner(my_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use my_os::println;
+use bootloader::{BootInfo, entry_point};
+use my_os::{println};
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
     my_os::init();
-    
-    use x86_64::registers::control::Cr3;
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
-
+  
     #[cfg(test)]
     test_main();
     println!("It did not crash!");
